1fda1c46f9096d87a1a07e7501e81e95935af911
Willem Helmet Pickleman <willemhelmet@gmail.com>
2026-01-10 15:17:05 -0500

    conductor(checkpoint): Checkpoint end of Phase 1 - Environment & Database Setup

---
+++ server/test-connection.js
@@ -0,0 +1,14 @@
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000");
+
+socket.on("connect", () => {
+  console.log("Connected to server!");
+  socket.disconnect();
+  process.exit(0);
+});
+
+socket.on("connect_error", (err) => {
+  console.error("Connection error:", err.message);
+  process.exit(1);
+});
