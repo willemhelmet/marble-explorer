d8c8577ac429006ddf6a1ec3a19895d79d36e06e
Willem Helmet Pickleman <willemhelmet@gmail.com>
2026-01-13 12:57:13 -0500

    build errror fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
     base: "/",
