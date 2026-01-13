e12e33c4a5fbb2936e1b8457334b7df3cf625676
Willem Helmet Pickleman <willemhelmet@gmail.com>
2026-01-13 12:42:38 -0500

    removed env check

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), "");
-
+export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: env.VITE_BASE_PATH || "/",
+    base: "/marble-explorer/",
   };
 });
+
