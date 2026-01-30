299606939e8884a9f36e5c998568485012998f63
Willem Helmet Pickleman <willemhelmet@gmail.com>
2026-01-29 19:05:40 -0500

    conductor(checkpoint): Phase 2 - Loader Stylization complete

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,51 @@ import { MobileControls } from "./components/ui/MobileControls.tsx";
 import { ControlsManager } from "./components/ControlsManager.tsx";
 import { useMemo, useEffect } from "react";
 import { socketManager } from "./services/socketManager";
+import { CSSProperties } from "react";
+
+const loaderStyles: { [key: string]: CSSProperties } = {
+  container: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    background: "#000",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    transition: "opacity 300ms ease",
+    zIndex: 1000,
+  },
+  inner: {
+    width: 300,
+    height: 3,
+    background: "#222",
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  bar: {
+    height: 3,
+    width: "100%",
+    background: "white",
+    transition: "transform 200ms",
+    transformOrigin: "left center",
+  },
+  data: {
+    display: "inline-block",
+    position: "relative",
+    fontVariantNumeric: "tabular-nums",
+    marginTop: "1em",
+    color: "#fff",
+    fontSize: "1.2rem",
+    fontFamily: "Karrik",
+    whiteSpace: "nowrap",
+    textTransform: "uppercase",
+    letterSpacing: "0.1em",
+  },
+};
 
 function App() {
   const isMobile = useMyStore((state) => state.isMobile);
@@ -23,6 +68,7 @@ function App() {
   const pause = useMyStore((state) => state.pause);
   const resume = useMyStore((state) => state.resume);
   const editingPortal = useMyStore((state) => state.editingPortal);
+  const displayName = useMyStore((state) => state.displayName);
 
   useEffect(() => {
     socketManager.connect();
@@ -93,7 +139,13 @@ function App() {
             {isMobile && <CameraControls smoothTime={0} />}
           </Canvas>
         </KeyboardControls>
-        <Loader />
+        <Loader
+          containerStyles={loaderStyles.container}
+          innerStyles={loaderStyles.inner}
+          barStyles={loaderStyles.bar}
+          dataStyles={loaderStyles.data}
+          dataInterpolation={(p) => `${p.toFixed(0)}%`}
+        />
         {/* <Stats /> */}
       </div>
     </>
